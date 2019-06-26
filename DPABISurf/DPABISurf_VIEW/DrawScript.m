58f0537b93e443b0e2def6018c9bfe664f5db2ad
sandywang <sandywang.rest@gmail.com>
2019-06-26 16:14:05 -0400

    Add basic function for yoke

--- DPABISurf/DPABISurf_VIEW/DrawScript.m
+++ DPABISurf/DPABISurf_VIEW/DrawScript.m
@@ -1,13 +1,17 @@
-[AxesObj, Fcn]=w_RenderSurf('/Users/ycg/Dropbox/ITraAllOnline/ITraAll/ITraDropBox/GoogleDrive/ITraActive/Programs/DPABI/Github/DPABI/DPABISurf/SurfTemplates/fsaverage5_lh_inflated.surf.gii');
-Fcn.AddOverlay('T2.gii'); % Your Overlay Gifti;
-Fcn.SetOverlayThres(1, -3, -1, 1, 3);
-Fcn.SetOverlayColorMap(1, colormap('jet(64)'), '');
-OneFig=Fcn.SaveMontage('L', 'Test_L.tif');
+% [AxesObj, Fcn]=w_RenderSurf('/Users/ycg/Dropbox/ITraAllOnline/ITraAll/ITraDropBox/GoogleDrive/ITraActive/Programs/DPABI/Github/DPABI/DPABISurf/SurfTemplates/fsaverage5_lh_inflated.surf.gii');
+% Fcn.AddOverlay('T2.gii'); % Your Overlay Gifti;
+% Fcn.SetOverlayThres(1, -3, -1, 1, 3);
+% Fcn.SetOverlayColorMap(1, colormap('jet(64)'), '');
+% OneFig=Fcn.SaveMontage('L', 'Test_L.tif');
+% 
+% [AxesObj, Fcn]=w_RenderSurf('/Users/ycg/Dropbox/ITraAllOnline/ITraAll/ITraDropBox/GoogleDrive/ITraActive/Programs/DPABI/Github/DPABI/DPABISurf/SurfTemplates/fsaverage5_rh_inflated.surf.gii');
+% Fcn.AddOverlay('T2.gii'); % Your Overlay Gifti;
+% Fcn.SetOverlayThres(1, -3, -1, 1, 3);
+% Fcn.SetOverlayColorMap(1, colormap('jet(64)'), '');
+% OneFig=Fcn.SaveMontage('R', 'Test_R.tif');
+% 
+% figure, montage({'Test_L.tif', 'Test_R.tif'})
 
-[AxesObj, Fcn]=w_RenderSurf('/Users/ycg/Dropbox/ITraAllOnline/ITraAll/ITraDropBox/GoogleDrive/ITraActive/Programs/DPABI/Github/DPABI/DPABISurf/SurfTemplates/fsaverage5_rh_inflated.surf.gii');
-Fcn.AddOverlay('T2.gii'); % Your Overlay Gifti;
-Fcn.SetOverlayThres(1, -3, -1, 1, 3);
-Fcn.SetOverlayColorMap(1, colormap('jet(64)'), '');
-OneFig=Fcn.SaveMontage('R', 'Test_R.tif');
-
-figure, montage({'Test_L.tif', 'Test_R.tif'})
+[AxesObj, Fcn]=w_RenderSurf('../SurfTemplates/fsaverage5_rh_inflated.surf.gii');
+%Fcn.MoveDataCursor([0, 0, 0]);
+Fcn.GetDataCursorPos()
