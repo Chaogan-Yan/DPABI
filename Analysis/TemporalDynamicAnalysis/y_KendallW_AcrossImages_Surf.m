676a0c24212334238a28d74ff905f5bbd5b32470
Zhikai Chang <33238154+czk10102@users.noreply.github.com>
2019-07-09 13:42:34 +0800

    Add files via upload

--- Analysis/TemporalDynamicAnalysis/y_KendallW_AcrossImages_Surf.m
+++ Analysis/TemporalDynamicAnalysis/y_KendallW_AcrossImages_Surf.m
@@ -1,20 +1,25 @@
 function [KendallW] = y_KendallW_AcrossImages_Surf(RaterImages_L, RaterImages_R, MaskData_L, MaskData_R, AResultFilename)
 % Calculate Kendall's W across images. (e.g., different raters or Test Re-Test)
-% FORMAT     [KendallW] = y_KendallW_AcrossImages(RaterImages, MaskData, AResultFilename)
+% FORMAT     [KendallW] = y_KendallW_AcrossImages_Surf(RaterImages_L, RaterImages_R, MaskData_L, MaskData_R, AResultFilename)
 % Input:
-% 	RaterImages     -	Cells of raters (nRater * 1 cells), each rater could be:
-%                       1. The directory of 3D image data
-%                       2. The filename of one 4D data file
-%                       3. a Cell (nFile * 1 cells) of filenames of 3D image data
-% 	MaskData		-   the mask file name or 3D mask matrix
+% 	RaterImages_L     -	For Left Hemishpere: Cells of raters (nRater * 1 cells), each rater could be:
+%                       1. The directory of 2D image data
+%                       2. The filename of one 1D data file
+%                       3. a Cell (nFile * 1 cells) of filenames of 1D image data
+% 	RaterImages_R     -	For Right Hemishpere: Cells of raters (nRater * 1 cells), each rater could be:
+%                       1. The directory of 2D image data
+%                       2. The filename of one 1D data file
+%                       3. a Cell (nFile * 1 cells) of filenames of 1D image data
+% 	MaskData_L		-   For Left Hemishpere: the mask file name or 1D mask matrix
+% 	MaskData_R		-   For Right Hemishpere: the mask file name or 1D mask matrix
 %	AResultFilename		the output filename
 
 % Output:
-%	KendallWBrain   -   The Kendall's W results
-%   Header          -   The NIfTI Header
+%	KendallW   -   The Kendall's W results
+%   Header          -   The GIfTI Header
 %	AResultFilename	the filename of Kendall's W result
 %___________________________________________________________________________
-% Written by YAN Chao-Gan 171001.
+% Written by YAN Chao-Gan 190704.
 % The R-fMRI Lab, Institute of Psychology, Chinese Academy of Sciences, Beijing, China
 % ycg.yan@gmail.com
 
@@ -26,24 +31,25 @@ fprintf('\n\tKendall''s W computation Start...\n');
 if length(RaterImages_L)~=length(RaterImages_R)
     error('The number of RaterImages of two hemispheres do not fit');
 end
-GHeader_L = gifti(RaterImages_L{1});
-GHeader_R = gifti(RaterImages_R{1});
-AllVolume_L=GHeader_L.cdata;
-AllVolume_R=GHeader_R.cdata;
+
+[AllVolume_L,VoxelSize,theImgFileList, GHeader_L] = y_ReadAll(RaterImages_L{1});
+[AllVolume_R,VoxelSize,theImgFileList, GHeader_R] = y_ReadAll(RaterImages_R{1});
 
 [nDimVertex_L, nDimTimePoints_L]=size(AllVolume_L);
 [nDimVertex_R, nDimTimePoints_R]=size(AllVolume_R);
 if nDimTimePoints_L~=nDimTimePoints_R
-    error('The TimePoints of two hemispheres do not fit');
+    error('The Time Points of two hemispheres do not fit');
 end
 nDimTimePoints=nDimTimePoints_L;
-nDimVertex=nDimVertex_L+nDimVertex_R;
-BrainSize = nDimVertex;
 
 if ischar(MaskData_L)
     if ~isempty(MaskData_L)
         MaskData_L=gifti(MaskData_L);
         MaskData_L=MaskData_L.cdata;
+        if size(MaskData_L,1)~=nDimVertex_L
+            error('The size of Left Hemishpere Mask (%d) doesn''t match the required size (%d).\n',size(MaskData_L,1), nDimVertex_L);
+        end
+        MaskData_L = double(logical(MaskData_L));
     else
         MaskData_L=ones(nDimVertex_L,1);
     end
@@ -53,6 +59,10 @@ if ischar(MaskData_R)
     if ~isempty(MaskData_R)
         MaskData_R=gifti(MaskData_R);
         MaskData_R=MaskData_R.cdata;
+        if size(MaskData_R,1)~=nDimVertex_R
+            error('The size of Right Hemishpere Mask (%d) doesn''t match the required size (%d).\n',size(MaskData_R,1), nDimVertex_R);
+        end
+        MaskData_R = double(logical(MaskData_R));
     else
         MaskData_R=ones(nDimVertex_R,1);
     end
@@ -62,28 +72,18 @@ MaskDataOneDim=reshape(MaskData,1,[]);
 MaskIndex = find(MaskDataOneDim);
 
 
-
-%RankSet = zeros(nDimTimePoints,length(MaskIndex),length(RaterImages));
 RankSet = repmat((zeros(length(MaskIndex),1)),[1,nDimTimePoints,length(RaterImages_L)]);
 
 for iRater = 1:length(RaterImages_L)
+    AllVolume_L = y_ReadAll(RaterImages_L{iRater});
+    AllVolume_R = y_ReadAll(RaterImages_R{iRater});
+    AllVolume=cat(1,AllVolume_L,AllVolume_R);
 
-    AllVolume_L = gifti(RaterImages_L{iRater});
-    AllVolume_L=AllVolume_L.cdata;
-    AllVolume_R = gifti(RaterImages_R{iRater});
-    AllVolume_R=AllVolume_R.cdata;
-    AllVolume=[AllVolume_L;AllVolume_R];
-    % Convert into 2D
-    AllVolume=reshape(AllVolume,[],nDimTimePoints)';
-    AllVolume=AllVolume(:,MaskIndex);
-    
+    AllVolume=AllVolume(MaskIndex,:);
     [R,TIEADJ] = tiedrank(AllVolume);
-    
-    
-    RankSet(:,:,iRater) = R';
-    
-end
 
+    RankSet(:,:,iRater) = R;
+end
 
 KendallW = zeros(nDimTimePoints,1);
 
