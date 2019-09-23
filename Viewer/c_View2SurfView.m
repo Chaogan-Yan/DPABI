d47e640aed01fbad53112d31c349304bb97bf314
Chaogan-Yan <ycg.yan@gmail.com>
2019-09-23 20:51:30 +0800

    DPABIV4.2

--- Viewer/c_View2SurfView.m
+++ Viewer/c_View2SurfView.m
@@ -2,20 +2,23 @@ function c_View2SurfView(OriginalName,Surf,Surf_Mask,P_Max,P_Min,N_Max,N_Min,Col
 %This is a function which is used to display the result of DPABI_VIEW by DPABISurf_VIEW.
 %FORMAT c_View2SurfView(OriginalName,Surf,Surf_Mask,P_Max,P_Min,N_Max,N_Min,ColorMap,eventdata)
 %Input:
-%     Surf - The cell which contains two names of surface files, the fisrt name is the left hemisphere while the second is the right hemisphere.
+%     OriginalName  - The Name in DPABI_VEW
+%     Surf          - The cell which contains two names of surface files, the fisrt name is the left hemisphere while the second is the right hemisphere.
+%     Surf_Mask     - The cell which contains two names of surface mask files, the fisrt name is the left hemisphere while the second is the right hemisphere.
 %     P_Max         - The maximum
-%                  - default: calculate from BrainVolume
+%                   - default: calculate from BrainVolume
 %     P_Min         - The positive minimum. Could be the positive threshold
-%                  - default: calculate from BrainVolume
+%                   - default: calculate from BrainVolume
 %     N_Max         - The negative maximum (maximum in absolute value)
-%                  - default: calculate from BrainVolume
+%                   - default: calculate from BrainVolume
 %     N_Min         - The negative minimum (minimum in absolute value). Could be the negative threshold
-%                  - default: calculate from BrainVolume
-
-%  eventdata - Handle to event object
+%                   - default: calculate from BrainVolume
+%     ColorMap      - The color map. Should be m by 3 color array.
+%     eventdata     - Handle to event object
 %___________________________________________________________________________
+% Key Laboratory of Behavioral Science and Magnetic Resonance Imaging Research Center, Institute of Psychology, Chinese Academy of Sciences, Beijing, China
 % Written by Zhikai Chang 190906.
-
+% Revised by YAN Chao-Gan, 190920.
 
 DPABISurfPath=fileparts(which('DPABISurf.m'));
 SurfTpmPath=fullfile(DPABISurfPath, 'SurfTemplates');
@@ -70,39 +73,10 @@ for i=1:2 %Generate the results on DPABISurf_VIEW
     DPABISurf_VIEW('OverlayNMaxEty_Callback',NMax(i,1),eventdata,guidata(NMax(i,1)));
     set(NMin(i,1),'String',N_Min);
     DPABISurf_VIEW('OverlayNMinEty_Callback',NMin(i,1),eventdata,guidata(NMin(i,1)));
-
     DPABISurf_VIEW('MontageBtn_Callback',1,eventdata,handles);
-
-%     f = getframe(Picture(i,1));
-%     Size=size(f.cdata);
-%     CData{i,1}=f.cdata(:,ceil(0.12*Size(2)):ceil(0.88*Size(2)),:);
-%     set(VP_Menu(i,1),'Value',3);
-%     DPABISurf_VIEW('ViewPointMenu_Callback',VP_Menu(i,1),eventdata,guidata(VP_Menu(i,1)));
-%     f = getframe(Picture(i,1));
-%     Size=size(f.cdata);
-%     CData{i,2}=f.cdata(:,ceil(0.12*Size(2)):ceil(0.88*Size(2)),:);
-%     set(VP_Menu(i,1),'Value',2);
-%     DPABISurf_VIEW('ViewPointMenu_Callback',VP_Menu(i,1),eventdata,guidata(VP_Menu(i,1)));
 end
 name=split(OriginalName,'.');
-Really_Name=strcat(name{1,1},'.jpg');
+Really_Name=strcat(name{1,1},'.tiff');
 h=figure;
-% montage({[Surf{2,1}(1:end-4),'_Montage.jpg'],[Surf{1,1}(1:end-4),'_Montage.jpg']});
 montage({[OriginalName(1:end-4),'_Surf_lh_Montage.jpg'],[OriginalName(1:end-4),'_Surf_rh_Montage.jpg']});
-set(0, 'CurrentFigure', h)
-h
-gcf
-saveas(gcf,Really_Name);
-% eval(['print -r300 -dtiff -noui ''',Really_Name,''';']); %YAN Chao-Gan, 140806.
-
-
-% cdata=cell2mat(CData);
-% imwrite(cdata,Really_Name);
-%saveas(gcf,'xxx.jpg','jpg');
-% for i=1:2 %adjust the colorbar
-%     handles=guidata(View{i,1});
-%     Fcn=handles.Fcn;
-%     Fcn.UpdateOverlay(1);
-% end
-
-
+print(h,'-r300','-dtiff','-noui',Really_Name);
